878052cb8aa37b08ddecddee624ff602d44cf546
mekeyace3-jpg <mekeyace3@gmail.com>
2025-12-10 17:03:38 +0900

    2025-12-10

---
+++ src/io/io18.java
@@ -0,0 +1,23 @@
+package io;
+//http://mekeyace.kbsn.or.kr/user.csv
+/*
+ 응용문제. 다음 프로세서를 실행하는 코드를 작성하시오. 고객데이터는 user.csv의 파일을
+ 이용하여 사용해야 합니다. 
+ 
+ [실행]
+ 통신사를 선택해 주세요 [SKT,KT,LGT,알뜰폰] : 
+ 잘못 입력시 : "해당 메뉴에 맞는 단어만 입력하세요" 다시 메뉴가 출력
+ 
+ [결과]
+ 통신사를 선택해 주세요 [SKT,KT,LGT,알뜰폰] : LGT
+ [김준수,신유빈,윤하빈,조선경,황인호]
+ */
+
+public class io18 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
